d79fe14c806dfbcc7ff56ced4dafed8711f5cb33
imimali <imre.mali@sectorlabs.ro>
2021-01-03 12:23:27 +0200

    Add steady state distribution calculator specs

--- seminar6/steadyP.m
+++ seminar6/steadyP.m
@@ -1,3 +1,12 @@
+% compute the steady state distribution of a stochastic process, if it exists
+% this essentially solves the system of equations 
+
+%|pi * P = pi
+%|sum(pi) = 1
+
+% the below method rearranges the first equation as 
+% pi (P -I) = O
+% and incorporates into the matrix the second equation(add a row of ones to both sides
 function A = steadyP(P)
     PP = [(P-eye(length(P))) ones(length(P),1)];
     A=[zeros(1,length(P)) 1]/PP;
