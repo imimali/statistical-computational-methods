70a4e4943a76099dde8b2dd8f27c5dac937da4d1
imimali <imre.mali@sectorlabs.ro>
2021-01-24 16:30:17 +0200

    ADd explanations to matbksqp

--- seminar6/matBkSQP.m
+++ seminar6/matBkSQP.m
@@ -5,8 +5,47 @@
 % capacity or size C
 % type - pass 'limited' to let the function know C is used as a maximum
 % capacity, and pass anything else if C is used as simple size
+% Note: if type is 'limited', that is, and C is used as a maximum capacity,
+% it should equal the actual capacity + 1, since the number of states
+% includes zero as well.
+% examples:
+
+% For the example from page 12 of lecture 9, we have 
+% matBkSQP(0.2,0.125,2,4 + 1,'limited') % note the 4 + 1
+% which gives 
+%    0.8000    0.2000         0         0         0
+%    0.1000    0.7250    0.1750         0         0
+%    0.0125    0.1781    0.6562    0.1531         0
+%         0    0.0125    0.1781    0.6562    0.1531
+%         0         0    0.0125    0.1781    0.8094
+
+% For the example from Seminar 6, ex. 2:
+% matBkSQP(1/8,1/10,2,3,'limited')
+%    0.8750    0.1250         0
+%    0.0875    0.8000    0.1125
+%    0.0088    0.1588    0.8325
+
+% for example from page 9 lecture 8, we have
+% matBkSQP(1/9, 1/2, 1, 9, 'something') % note how limited was not passed
+% and 9 is used as the matrix size of an infinite X infinite matrix. It
+% gives
+
+%    0.8889    0.1111         0         0         0         0         0         0         0
+%    0.4444    0.5000    0.0556         0         0         0         0         0         0
+%         0    0.4444    0.5000    0.0556         0         0         0         0         0
+%         0         0    0.4444    0.5000    0.0556         0         0         0         0
+%         0         0         0    0.4444    0.5000    0.0556         0         0         0
+%         0         0         0         0    0.4444    0.5000    0.0556         0         0
+%         0         0         0         0         0    0.4444    0.5000    0.0556         0
+%         0         0         0         0         0         0    0.4444    0.5000    0.0556
+%         0         0         0         0         0         0         0    0.4444    0.5000%
+
+
+
+
 
 function P = matBkSQP(pa,ps,k,C, type)
+
 P = zeros(C,C);
 p_i_i = @(n) pa * nchoosek(n,1) * ps * ((1-ps)^(n-1)) + (1-pa)*((1-ps)^(n));
 p_i_ip1 = @(n) pa*(1-ps)^n;
@@ -31,3 +70,4 @@ P(1,2) = pa;
 if(strcmp(type,'limited'))
     P(C,C) = n *pa * ps*((1-ps)^(n-1))+(1-ps)^n;
 end
+
